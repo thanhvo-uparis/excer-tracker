cfb079f612aba4772a87c93e7f36869b5a4059b9
thanh vo.u-paris <72513778+thanhvo-uparis@users.noreply.github.com>
2021-12-09 13:11:31 +0100

    connected to DB

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,27 @@
 const express = require('express');
-const app = express();
+const cors = require('cors');
+const mongoose = require('mongoose');
 
+require('dotenv').config();
+ 
+const app = express();
 const PORT = 5000;
-app.listen(PORT, () => {
-    console.log(`Server is running at the port ${PORT}`);
+
+
+app.use(cors());
+app.use(express.json());
+
+const URI = "mongodb+srv://thanhvo:node123@cluster0.dha26.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+app.get("/", (request, response) => {
+        response.send("get a réussi!");
+})
+
+mongoose.connect(URI, {useNewUrlParser: true, useUnifiedTopology: true}).then(() => {
+    console.log("Contected to DB");
+    app.listen(PORT, () => {
+        console.log(`Server is running at the port ${PORT}`);
+    });
+}).catch((err) => {
+    console.log('err', err);
 });
